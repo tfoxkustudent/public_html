c75236eff8f3e28365221c2060a5c1901994f089
Tim Fox <tfoxku@gmail.com>
2018-09-29 16:26:26 -0500

    1

--- customerBackend.php
+++ customerBackend.php
@@ -5,8 +5,9 @@ $answer2 = $_POST["password"];
 $answer3 = $_POST["item1"];
 $answer4 = $_POST["item2"];
 $answer5 = $_POST["item3"];
-$answer5 = $_POST["shipping"];
+$answer6 = $_POST["shipping"];
 $total = 0;
+$subtotoal = 0;
 
 
 echo "<strong>";
@@ -29,7 +30,41 @@ echo "</tr>";
   echo "<td>Cost Per Item</td>";
   echo "<td>Subtotal</td>";
   echo "</tr>";
-		echo "</table>";
+    echo "<tr>";
+    echo "<td>Bananas</td>";
+    echo "<td>$answer3</td>";
+    echo "<td>$3.00</td>";
+    echo "<td>$subtotal</td>";
+    echo "</tr>";
+      echo "<tr>";
+      echo "<td>Apples</td>";
+      echo "<td>$answer4</td>";
+      echo "<td>$2.00</td>";
+      echo "<td>$subtotal</td>";
+      echo "</tr>";
+        echo "<tr>";
+        echo "<td>Oranges</td>";
+        echo "<td>$answer5</td>";
+        echo "<td>$1.00</td>";
+        echo "<td>$subtotal</td>";
+        echo "</tr>";
+          echo "<tr>";
+          echo "<td>Shipping</td>";
+          echo "<td>$answer6</td>";
+          echo "<td></td>";
+          echo "<td>$subtotal</td>";
+          echo "</tr>";
+          echo "<tr>";
+          echo "<td>Total Cost</td>";
+          echo "<td></td>";
+          echo "<td></td>";
+          echo "<td>$total</td>";
+          echo "</tr>";
+
+
+
+
+    echo "</table>";
 
 
 
