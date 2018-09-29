b4697d85321220597d41f3902a713eea3a920de2
Tim Fox <tfoxku@gmail.com>
2018-09-29 15:36:39 -0500

    started on backend

---
+++ customerBackend.php
@@ -0,0 +1,24 @@
+<?php
+<PHP> include('style.css'); <PHP>
+$answer1 = $_POST["username"];
+$answer2 = $_POST["password"];
+$answer3 = $_POST["item1"];
+$answer4 = $_POST["item2"];
+$answer5 = $_POST["item3"];
+$answer5 = $_POST["shipping"];
+$total = 0;
+
+echo "<strong>";
+echo "Welcome Back ";
+echo $answer1;
+echo "</strong>";
+echo "<br>";
+echo "Your password is: ";
+echo $answer2;
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+
+
+?>
