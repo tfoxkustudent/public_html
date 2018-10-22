56b39b0d6ae3c623c96da19c8c951f92d3c6b491
Tim Fox <tfoxku@gmail.com>
2018-10-22 10:48:29 -0500

    finished lab 6

--- CreatePosts.php
+++ CreatePosts.php
@@ -1,24 +1,17 @@
 <?php
 $mysqli = new mysqli("mysql.eecs.ku.edu", "t399f699", "eecs448", "t399f699");
-
 /* check connection */
 if ($mysqli == false) {
-
     echo("Connect failed: \n". $mysqli->connect_error);
-
 }
-
 $existing_user = $mysqli->real_escape_string($_REQUEST['user']);
 $content = $mysqli->real_escape_string($_REQUEST['content']);
 $query = "SELECT * FROM `Users` WHERE `user_id`=`$existing_user` ";
     $result = mysql_query($query);
-
 if (!mysql_num_rows($result)){
     $sql = "INSERT INTO Posts (author_id, content)
     VALUES ('$existing_user','$content')";
-
     if ($mysqli->query($sql) == true) {
-
             //Check if entry was entered into DB
             echo "New record created successfully";
           } else {
@@ -28,9 +21,6 @@ if (!mysql_num_rows($result)){
         else {
           echo "ID doesn't exist... go to create user page";
         }
-
-
-
 /* close connection */
 $mysqli->close();
 ?>
---
+++ ViewUserPosts.php
@@ -0,0 +1,30 @@
+<?php
+$con=mysqli_connect("mysql.eecs.ku.edu","t399f699","eecs448","t399f699");
+// Check connection
+if (mysqli_connect_errno())
+{
+echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$result = mysqli_query($con,"SELECT * FROM Posts");
+
+echo "<table border='1'>
+<tr>
+<th>User ID</th>
+<th>Post</th>
+
+</tr>";
+
+while($row = mysqli_fetch_array($result))
+{
+echo "<tr>";
+echo "<td>" . $row['author_id'] . "</td>";
+echo "<td>" . $row['content'] . "</td>";
+
+echo "</tr>";
+}
+echo "</table>";
+
+mysqli_close($con);
+
+ ?>
